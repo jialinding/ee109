63bd5faa9912d3e437025239d7ab4643177c1369
Jialin Ding <djialin09@gmail.com>
2017-05-10 01:35:06 -0700

    Finish basic prototype pipeline

--- prototype/stabilizer.py
+++ prototype/stabilizer.py
@@ -3,61 +3,76 @@ import cv2
 
 threshold = 3
 
+
 def detect_features(frame):
-    orb = cv2.ORB()
-    kp = orb.detect(frame, None)
-    return cv2.drawKeypoints(frame, kp, color=(255,0,0))
+	orb = cv2.ORB_create()
+	kp = orb.detect(frame, None)
+	return kp
+
 
 def match_features(fr1, fr2, f1, f2):
-    f1, d1 = orb.compute(fr1, f1)
-    f2, d2 = orb.computer(fr2, f2)
+	orb = cv2.ORB_create()
+	f1, d1 = orb.compute(fr1, f1)
+	f2, d2 = orb.compute(fr2, f2)
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    temp = bf.knnmatch(d1, d2, k=2)
-    matches = []
-    for m,n in temp:
-        if m.distance < 0.8*n.distance:
-            matches.append([m])
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+	temp = bf.knnMatch(d1, d2, k=2)
+	matches = []
+	for m,n in temp:
+		if m.distance < 0.8*n.distance:
+			matches.append(m)
 
-    return matches
-    # return cv2.drawMatches(fr1, f1, fr1, f2, matches[:10], flags=2)
+	pts1 = []
+	pts2 = []
+	for match in matches:
+		pts1_ind = match.queryIdx
+		pts2_ind = match.trainIdx
+		pts1_kp = f1[pts1_ind]
+		pts2_kp = f2[pts2_ind]
+		pts1.append(pts1_kp.pt)
+		pts2.append(pts2_kp.pt)
 
-def estimate_transform(matches):
-	pts1 = matches[0]
-	pts2 = matches[1]
-	M = cv2.getAffineTransform(pts1, pts2)
+	return np.asarray(pts1), np.asarray(pts2)
+	# return cv2.drawMatches(fr1, f1, fr1, f2, matches, np.zeros((1,1)), flags=2)
+
+
+def estimate_transform(pts1, pts2):
+	M, _ = cv2.findHomography(pts1, pts2, cv2.RANSAC, 5.0)
 	return M
 
 
 def stabilize_video(video):
+	cv2.ocl.setUseOpenCL(False)
 	cap = cv2.VideoCapture(video)
 
 	ret, first_frame = cap.read()
 	first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 	prev_frame = first_frame
 	prev_features = detect_features(first_frame)
-	rows, cols = first_frame[:,:,0].shape
+	rows, cols = first_frame.shape
 
-	cumulative_transform = np.eye(2, 3)
+	cumulative_transform = np.identity(3)
 
 	while(cap.isOpened()):
 		ret, frame = cap.read()
+		if ret == False:
+			# video is over
+			break
 
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 		features = detect_features(frame)
-		matches = match_features(prev_frame, frame, prev_features, features)
-		transform = estimate_transform(matches)
+		pts1, pts2 = match_features(frame, prev_frame, features, prev_features)
+		transform = estimate_transform(pts1, pts2)
 
 		# Append to cumulative transform
-		temp_cum_transform = np.identity(3)
-		temp_cum_transform[:2,:] = cumulative_transform
-		temp_transform = np.identity(3)
-		temp_transform[:2,:] = transform
-		cumulative_transform = temp_cum_transform.dot(temp_transform)[:2,:]
+		temp_cum_transform = np.identity(4)
+		temp_cum_transform[:3,:3] = cumulative_transform
+		temp_transform = np.identity(4)
+		temp_transform[:3,:3] = transform
+		cumulative_transform = temp_transform.dot(temp_cum_transform)[:3,:3]
 
-		stabilized = cv2.warpAffine(first_frame, cumulative_transform, (cols, rows))
-		# stabilized = gray
+		stabilized = cv2.warpPerspective(frame, cumulative_transform, (cols, rows))
 
 		cv2.imshow('frame', stabilized)
 
--- prototype/test.py
+++ prototype/test.py
@@ -1,7 +1,11 @@
 from stabilizer import *
+from timeit import default_timer as timer
 
 def main():
+	start = timer()
 	stabilize_video('sample_video.mp4')
+	end = timer()
+	print "Time: {} s".format(end-start)
 
 
 if __name__ == "__main__":
