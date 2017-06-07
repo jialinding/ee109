2d9323529e8ce3bf6fc84128eba27fe28427548d
Jialin Ding <djialin09@gmail.com>
2017-06-06 23:26:24 -0700

    Debug is working

--- Debug.scala
+++ Debug.scala
@@ -12,7 +12,7 @@ object Debug extends SpatialApp {
   val t = 50  // FIXME
   val n = 12
   val lb_height = 7
-  val lb_width = 640
+  val lb_width = 128
   val sr_height = 7
   val sr_width = 7
 
@@ -29,8 +29,9 @@ object Debug extends SpatialApp {
 
   @virtualize
   def detect_FAST(): Unit = {
-    val imgIn  = StreamIn[Pixel24](target.VideoCamera)
-    val imgOut = StreamOut[Pixel24](target.VGA)
+    val imgIn  = StreamIn[Pixel16](target.VideoCamera)
+    // val imgOut = StreamOut[Pixel24](target.VGA)
+    val imgOut = BufferedOut[Pixel16](target.VGA)
     // val imgOut = StreamOut[Pixel16](target.VGA)
 
     Accel {
@@ -56,58 +57,39 @@ object Debug extends SpatialApp {
 
       val sr = RegFile[Int16](sr_height, sr_width)
       val fifoIn = FIFO[Int16](C)
-      val fifoOut = FIFO[Int16](C)
-      val lb = LineBuffer[Int16](lb_height, lb_width)
+      // val fifoOut = FIFO[Int16](C)
+      val lb = LineBuffer[Int16](lb_height, C)
 
-      // val row = 0
-      // val col = 0
+      // // val row = 0
+      // // val col = 0
 
       val curr = Reg[Int16](0)
       val is_feature = Reg[Int16](0)
       val running_count = Reg[Int16](0)
 
-      println("Start of stream")
+      // println("Start of stream")
 
       Stream(*) { _ =>
-        val pixel = imgIn.value()
-        val grayscale_pixel = (pixel.b.to[Int16] + pixel.g.to[Int16] + pixel.r.to[Int16]) / 3
-        fifoIn.enq(grayscale_pixel)
-
-        // if (col == 639) {
-        //  col = 0
-        //  if (row == 479) {
-        //      row = 0
-        //  } else {
-        //      row = row + 1
-        //  }
-        // } else {
-        //  col = col + 1
-        // }
-
-        Foreach(0 until R, 0 until C){ (r, c) => 
-          Sequential {
-            val grayscale_pixel = fifoIn.deq()
+        Foreach(0 until R){ r => 
+          Sequential.Foreach(0 until C){ _ =>
+            val pixel = imgIn.value()
+            val grayscale_pixel = (pixel.b.to[Int16]*8 + pixel.g.to[Int16]*4 + pixel.r.to[Int16]*8) / 3
             lb.enq(grayscale_pixel)
+            fifoIn.enq(grayscale_pixel)
+          }
+          Sequential.Foreach(0 until C){ c =>
+            // println("loop " + r + " " + c)
+            val grayscale_pixel = fifoIn.deq()
+            
             Foreach(0 until sr_height){ i =>
               sr(i, *) <<= lb(i, c)
             }
 
-            // println("row " + r + " col " + c + " grayscale " + grayscale_pixel)
-
-            // if (r == 113 && c == 77) {
-            //   // println("grayscale")
-            //   println("grayscale " + grayscale_pixel)
-            // }
-
             val ring_values = RegFile[Int16](32)
 
-            Foreach(0 until 16){ i =>
+            Sequential.Foreach(0 until 16){ i =>
               val ring_pixel = sr_coord(i)
-              println(ring_pixel.x.to[Index] + " " + ring_pixel.y.to[Index])
               val ring_pixel_val = sr(ring_pixel.x.to[Index], ring_pixel.y.to[Index])
-              // if (r == 113 && c == 77) {
-              //   println(ring_pixel_val)
-              // }
               Sequential {
                 if (ring_pixel_val < grayscale_pixel - t) {
                     ring_values(i) = -1
@@ -126,7 +108,7 @@ object Debug extends SpatialApp {
             running_count := 1
             curr := ring_values(0)
             is_feature := 0
-            Foreach(1 until 32){ i =>
+            Sequential.Foreach(1 until 32){ i =>
               if (ring_values(i) == curr.value) {
                   running_count := running_count.value + 1
               } else {
@@ -136,13 +118,13 @@ object Debug extends SpatialApp {
 
               if (running_count.value == 12.to[Int16] && curr.value != 0.to[Int16]) {
                   is_feature := 1
-                  println(r + " " + c + " " + ring_values(0) + " " + ring_values(1) + " " +
-                    ring_values(2) + " " + ring_values(3) + " " + ring_values(4) + " " +
-                    ring_values(5) + " " + ring_values(6) + " " + ring_values(7) + " " +
-                    ring_values(8) + " " + ring_values(9) + " " + ring_values(10) + " " +
-                    ring_values(11) + " " + ring_values(12) + " " + ring_values(13) + " " +
-                    ring_values(14) + " " + ring_values(15))
-                  println(" grayscale " + grayscale_pixel)
+                  // println(r + " " + c + " " + ring_values(0) + " " + ring_values(1) + " " +
+                  //   ring_values(2) + " " + ring_values(3) + " " + ring_values(4) + " " +
+                  //   ring_values(5) + " " + ring_values(6) + " " + ring_values(7) + " " +
+                  //   ring_values(8) + " " + ring_values(9) + " " + ring_values(10) + " " +
+                  //   ring_values(11) + " " + ring_values(12) + " " + ring_values(13) + " " +
+                  //   ring_values(14) + " " + ring_values(15))
+                  // println(" grayscale " + grayscale_pixel)
                   // TODO: does Spatial have break statement?
               }
             }
@@ -153,16 +135,26 @@ object Debug extends SpatialApp {
             //   fifoOut.enq(grayscale_pixel)
             // }
 
-            fifoOut.enq(mux[Int16](is_feature.value == 1.to[Int16], 255, grayscale_pixel))
+            // fifoOut.enq(mux[Int16](is_feature.value == 1.to[Int16], 255, grayscale_pixel))
+            val pixel_value = mux[Int16](is_feature.value == 1.to[Int16], 255, grayscale_pixel)
+            // imgOut(r, c) = Pixel24(pixel_value(7::0).as[UInt8],
+            //   pixel_value(7::0).as[UInt8],
+            //   pixel_value(7::0).as[UInt8])
+            imgOut(r, c) = Pixel16(pixel_value(7::3).as[UInt5],
+              pixel_value(7::2).as[UInt6],
+              pixel_value(7::3).as[UInt5])
+            // imgOut(r, c) = Pixel24(grayscale_pixel(7::0).as[UInt8],
+            //   grayscale_pixel(7::0).as[UInt8],
+            //   grayscale_pixel(7::0).as[UInt8])
           }
         }
         
         // TODO: This step should be performed outside of the Foreach loop over the whole frame. You will need to dequeue pixels from fifOut, and send it to imgOut (The StreamOut port).
         // YOUR CODE HERE:
-        val pixel_value = fifoOut.deq()
-        imgOut := Pixel24(pixel_value(7::0).as[UInt8],
-          pixel_value(7::0).as[UInt8],
-          pixel_value(7::0).as[UInt8])
+        // val pixel_value = fifoOut.deq()
+        // imgOut := Pixel24(pixel_value(7::0).as[UInt8],
+        //   pixel_value(7::0).as[UInt8],
+        //   pixel_value(7::0).as[UInt8])
         // imgOut := Pixel16(pixel_value(7::3).as[UInt5],
         //   pixel_value(7::2).as[UInt6],
         //   pixel_value(7::3).as[UInt5])
